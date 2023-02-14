277152ae9e3b4f9586c76ff29e66b7ef6a140b77
Edmilson <edmilson@okn.com.br>
2023-02-14 17:32:44 -0300

    alterações no estilo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="container">
     <header class="header">
-      <h1 class="header__title">Minhas Tarefas</h1>
-      <button class="header__add-task-btn" @click="showModal = true">Adicionar Tarefa</button>
+      <h1 class="header__title">Konecta</h1>
     </header>
     <main class="main">
+      <button class="add-task-btn" @click="showModal = true">Adicionar Tarefa</button>
       <ul class="task-list">
         <li v-for="task in tasks" :key="task.id" class="task-list__item">
-          <input type="checkbox" v-model="task.completed" />
           <span :class="{ 'task-list__task--completed': task.completed }">{{ task.name }}</span>
+          <input type="checkbox" v-model="task.completed" />
           <button class="task-list__edit-task-btn" @click="showModal = true; selectedTask = task">Editar</button>
         </li>
       </ul>
@@ -20,8 +20,8 @@
         <input class="modal__input" v-model="form.name" />
       </form>
       <div class="modal__actions">
-        <button class="modal__cancel-btn" @click="showModal = false">Cancelar</button>
         <button class="modal__save-btn" @click="saveTask">{{ selectedTask ? 'Salvar' : 'Adicionar' }}</button>
+        <button class="modal__cancel-btn" @click="showModal = false">Cancelar</button>
       </div>
     </modal>
   </div>
@@ -41,7 +41,7 @@ export default {
       selectedTask: null,
       form: {
         name: ''
-      }
+      },
     };
   },
   methods: {
@@ -76,8 +76,125 @@ export default {
 };
 </script>
 
+<style>
+  *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  .header{
+    display: flex;
+    flex-flow: column wrap;
+    align-items: flex-start;
+    font-weight: bold;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    padding: 1.5em 0.5em 1.5em 1.5em;
+    background-color: #355C7D;
+    width: 100%;
+    color: white;
+  }
+  .header__title{
+    font-size: 1.5em;
+    background-color: transparent;
+  }
+  .add-task-btn{
+    font-size: 1.5em;
+    border: 0px;
+    margin: 0.5em 0 0 0;
+    padding: 0.5em;
+    cursor: pointer;  
+    background-color: #C06C84;
+    box-shadow: 0px 0px 2px 2px #F67280;
+    width: 80%;
+    color: white;
+    border-radius: 1.5em;
+  }
+  .main{
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: linear-gradient(#F8B195, #C06C84);
+    background-size: cover;
+    color: white;   
+  }
+  .task-list{
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    padding: 1.4em;
+  }
 
+  .task-list__item{
+    display: flex;
+    align-items: center;
+    align-self: flex-start;
+    padding: 1.2em;
+    font-size: 1.2em;
+  }
 
+  .task-list__edit-task-btn{
+    display: flex;
+    flex-direction: row;
+    margin-left: 10px;
+    background: transparent; 
+    color: rgb(255, 255, 255);
+    border: none;
+    border-radius: 1.5em;    
+  }
 
+  .modal__title{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-weight: bold;
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    padding: 25px 0 25px 0;
+    background-color: #355C7D;
+    width: 100%;
+    color: rgb(255, 255, 255);
+    font-size: 2em;
+  }
 
-
+  .modal__form{
+    font-family:Verdana, Geneva, Tahoma, sans-serif;
+    padding: 1em;
+    background: linear-gradient(#F8B195, #C06C84);
+    color: rgb(255, 255, 255);
+  }
+  .modal__label{
+    background-color: transparent;
+  }
+  .modal__input{
+    padding: 0.5em;
+    margin: 1em;
+    border-radius: 1em;
+    background-color: #C06C84;
+    color: rgb(255, 255, 255);
+    border: none;
+    
+  }
+  .modal__actions{
+    display: flex;
+    padding: 1em;
+    justify-content: flex-end;
+    background: linear-gradient(#C06C84, #6C5B7B);   
+    color: white;  
+  }
+  .modal__save-btn{
+    background-color: #C06C84;
+    border-radius: 1em;
+    color: rgb(255, 255, 255); 
+    padding:0.5em;
+    margin: 0.5em;
+  }
+  .modal__cancel-btn{
+    background-color: #C06C84;
+    border-radius: 1em;
+    color: rgb(255, 255, 255); 
+    padding:0.5em;
+    margin: 0.5em;
+  }
+</style>
